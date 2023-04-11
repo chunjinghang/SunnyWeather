8c377941b77eb9ae43c85e5d435a35698633a279
Tony <2438245652@qq.com>
2023-04-11 20:08:31 +0800

    实现搜索全球城市数据功能

--- app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -1,4 +1,18 @@
 package com.sunnyweather.android
 
-class SunnyWeatherApplication {
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication : Application() {
+    companion object{
+        const val TOKEN  = "MZgjXzP1gBkkT3hh"
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
 }
--- app/src/main/java/com/sunnyweather/android/logic/model/PlaceResponse.kt
+++ app/src/main/java/com/sunnyweather/android/logic/model/PlaceResponse.kt
@@ -1,4 +1,15 @@
 package com.sunnyweather.android.logic.model
 
-class PlaceResponse {
-}
+import android.location.Location
+import com.google.gson.annotations.SerializedName
+
+//定义数据模型
+data class PlaceResponse(val status: String, val places: List<Place>)
+
+data class Place(
+    val name: String, val location: Location,
+    @SerializedName("formatted_address") val address: String
+)
+
+data class Location(val lng: String, val lat: String)
+
